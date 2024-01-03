4230a0c86f243643b4f3db7ed9bbd0c37edef7d6
fahreyad <fahreyad.hossain@brainstation-23.com>
2024-01-03 16:01:29 +0600

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstace = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MONGODB connected !! DB HOST: ${connectionInstace.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
